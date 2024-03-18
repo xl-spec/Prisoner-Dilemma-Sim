0192ea170a48b7e25f9f55a3ddde84eb79f97883
xl-spec <augurship@gmail.com>
2024-03-18 13:35:40 -0400

    updated gamestate to better reflect prisoner's dilemma problem

--- Gamestate.py
+++ Gamestate.py
@@ -3,8 +3,9 @@ from Character import Character
 from GptAPI import GptAPI
 from Interpret import Interpret
 
+
 class Gamestate:
-    def __init__(self, yy_out, yn_out, ny_out, nn_out):
+    def __init__(self, yy_out, yn_out, ny_out, nn_out): # Gamestate(-2, -3, 0, -1)
         self.character_list = [Character(100, 0, 0, 0, 0), Character(100, 0, 0, 0, 0)]
         self.Gpt = GptAPI()
         self.round = 0
@@ -13,12 +14,13 @@ class Gamestate:
         self.yn_out = yn_out
         self.ny_out = ny_out
         self.nn_out = nn_out
+        self.turnRes = []
 
     def initializeSystem(self):
         for character in self.character_list:
             character.message_list.append({
                 "role": "system",
-                "content": f"You are a character playing a game against another player with the following: \nRules:\n- You and your opponent choose the response at the same time with the reponse containing ONLY with the string of 'y' for yes and 'n' for no. These are your only two options.\n- If you and opponent both chooses n, you both lose some gold\n- If you and opponent both chooses y, you both a lot of gold\n- If you choose y and your opponent chooses n, you lose no gold while your opponent loses a lot of gold\n- If you choose n and your opponent chooses y, you lose a lots of gold while your opponent lose no gold\nObjective: \n- Save as much gold as possible\n\nYou will be updated on \n"
+                "content": f"You are a character that is given 100 gold at the start of the game. You will be playing a game against another player with the following: \nRules:\n- You and your opponent choose the response at the same time with the reponse containing ONLY with the string of 'y' for yes and 'n' for no. These are your only two options.\n- If you and opponent both chooses n, you both lose some gold\n- If you and opponent both chooses y, you both a lot of gold\n- If you choose y and your opponent chooses n, you lose no gold while your opponent loses a lot of gold\n- If you choose n and your opponent chooses y, you lose a lots of gold while your opponent lose no gold\nObjective: \n- Save as much gold as possible\n\nYou will be given the following information:\n- Current gamestate \n- Gold remaining\n- A  culmination of your performance \n- You will not be given the history of your past decisions\n\n"
                 })
             character.message_list.append({ # i will shorted this code later in life, some repetition that idk how to get rid of
                 "role": "user",
@@ -35,29 +37,32 @@ class Gamestate:
         for character in self.character_list:
             response = self.Gpt.getResponse(character.message_list)
             res.append(response.choices[0].message.content)
-        
-        if res == ["y", "y"]:
+        self.turnRes = res
+    
+    def updateCharacters(self):
+        # Key: -2, -3, 0, -1
+        if self.turnRes == ["y", "y"]:
             self.character_list[0].money += self.yy_out
             self.character_list[1].money += self.yy_out
             self.character_list[0].yTie += 1
             self.character_list[1].yTie += 1
-        elif res == ["n", "n"]:
+        elif self.turnRes == ["n", "n"]:
             self.character_list[0].money += self.nn_out
             self.character_list[1].money += self.nn_out
             self.character_list[0].nTie += 1
             self.character_list[1].nTie += 1
-        elif res == ["y", "n"]:
+        elif self.turnRes == ["y", "n"]:
             self.character_list[0].money += self.yn_out
-            self.character_list[1].money -= self.ny_out
+            self.character_list[1].money += self.ny_out
             self.character_list[0].won += 1
             self.character_list[1].loss += 1
-        elif res == ["n", "y"]:
-            self.character_list[0].money -= self.ny_out
+        elif self.turnRes == ["n", "y"]:
+            self.character_list[0].money += self.ny_out
             self.character_list[1].money += self.yn_out
             self.character_list[0].loss += 1
             self.character_list[1].won += 1
 
-        print(f"res: {res}")
+        print(f"res: {self.turnRes}")
         self.round += 1
        
     def printGameState(self):
@@ -74,9 +79,10 @@ class Gamestate:
         
     def runOneTurn(self):
         self.runResponse()
+        self.updateCharacters()
         self.updateMessageList()
 
-runner = Gamestate(1, 3, -3, 2) # 
+runner = Gamestate(-2, -3, 0, -1)
 runner.initializeSystem()
 runner.runOneTurn()
 runner.runOneTurn()
